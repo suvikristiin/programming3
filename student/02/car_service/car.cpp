126fd5518c54efe6fa72e83dce36c1aa3cc028ae
Suvi Vehmaanperä <suvi.vehmaanpera@tuni.fi>
2026-03-19 18:55:38 +0200

    add solution to 5.7.2

--- student/02/car_service/car.cpp
+++ student/02/car_service/car.cpp
@@ -1,2 +1,133 @@
 #include "car.hh"
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+
+Car::Car()
+{
+
+}
+
+Car::Car(const std::string &brand, const std::string &model, const std::string &color, const std::string &regNum) :
+    brand_(brand), model_(model), color_(color), registerNum_(regNum)
+{
+    kilometresDriven_ = 0;
+    service_data_ = make_unique<ServiceData>();
+}
+
+Car::Car(const Car &other) :
+    brand_(other.brand_), model_(other.model_), color_(other.color_)
+{
+    kilometresDriven_ = 0;
+    service_data_ = make_unique<ServiceData>();
+}
+
+Car::~Car()
+{
+
+}
+
+Car &Car::operator=(const Car &other)
+{
+    if(this != &other)
+    {
+        service_data_.reset();
+        brand_ = other.brand_;
+        model_ = other.model_;
+        color_ = other.color_;
+        registerNum_ = other.registerNum_;
+        kilometresDriven_ = other.kilometresDriven_;
+
+        if(kilometresDriven_ != 0)
+        {
+            service_data_ = make_unique<ServiceData>(*(other.service_data_));
+        } else {
+            service_data_ = make_unique<ServiceData>();
+        }
+    }
+    return *this;
+}
+
+void Car::setRegisterNum(const std::string &regNum)
+{
+    registerNum_ = regNum;
+}
+
+void Car::setColor(const std::string &color)
+{
+    color_ = color;
+}
+
+std::string Car::brand() const
+{
+    return brand_;
+}
+
+std::string Car::model() const
+{
+    return model_;
+}
+
+std::string Car::color() const
+{
+    return color_;
+}
+
+std::string Car::registerNum() const
+{
+    return registerNum_;
+}
+
+int Car::kilometresDriven() const
+{
+    return kilometresDriven_;
+}
+
+const ServiceData &Car::serviceData() const
+{
+    return *service_data_;
+}
+
+void Car::drive(int kilometres)
+{
+    kilometresDriven_ += kilometres;
+}
+
+bool Car::service(const Service &service)
+{
+    return service_data_->makeService(service, kilometresDriven_);
+}
+
+bool Car::service(const std::string &date, const std::vector<std::string> &operations)
+{
+    Service newservice = {date, operations};
+    return service_data_->makeService(newservice, kilometresDriven_);
+}
+
+void Car::print() const
+{
+    cout << registerNum_ << " : " << brand_ << " : " << model_ << " : " << color_ << endl;
+
+    service_data_->print();
+      cout <<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- student/02/car_service/servicedata.cpp
+++ student/02/car_service/servicedata.cpp
@@ -1,2 +1,49 @@
 #include "servicedata.hh"
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+ServiceData::ServiceData()
+{
+    map<int, Service> services_;
+}
+
+ServiceData::~ServiceData()
+{
+
+}
+
+bool ServiceData::makeService(const Service &service, int kilometres)
+{
+
+    for ( pair<int, Service> tietopari : services_ ) {
+        if ( tietopari.first == kilometres ) {
+            cout <<"Error: already serviced with the same kilometres" << endl;
+            return false;
+        }
+    }
+
+    services_.insert( { kilometres, service } );
+
+    return true;
+}
+
+void ServiceData::print() const
+{
+
+    for ( pair<int, Service> tietopari : services_ ) {
+        cout << "* Kilometres driven " << tietopari.first << " until " << tietopari.second.date_ << endl;
+
+        for (string operation : tietopari.second.operations_) {
+            cout << "  --- " << operation << endl;
+        }
+    }
+}
+
+const std::map<int, Service> &ServiceData::getServices() const
+{
+    return services_;
+}
+
 
